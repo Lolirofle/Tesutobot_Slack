e7aeb0379bc36c9b580a52c8a9e47bb86d55c819
Lolirofle <lolipopple@hotmail.com>
2015-12-22 06:24:17 +0100

    Commenting

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from slacker import Slacker
 import json
 import sys
@@ -18,13 +20,18 @@ class Server(object):
 		self.connected = False
 
 	def rtm_connect(self):
+		""" Connects using the Real Time Messaging method """
+
+		# Requests RTM, and error checks
 		rtm = slack.rtm.start()
 		if not rtm.successful:
 			raise SlackRtmConnectionError(rtm.error)
 
+		# Store response data
 		self.data = rtm.body
 #		print(json.dumps(self.data,indent=4,sort_keys=True))
 
+		# Connect to the websocket server for receiving RTM data
 		self.websocket = websocket.create_connection(self.data['url'])
 #		self.websocket.sock.setblocking(0)
 		self.connected = True
@@ -34,17 +41,23 @@ class Server(object):
 			raise SlackNotConnectedError
 
 		try:
+			# For each received message (Waiting when not receiving)
 			while True:
 				message = json.loads(self.websocket.recv())
+				print(message)
+
+				# Match message type
 				if message['type']=="message":
+					# Notification to the bot from someone?
 					if message['text'].startswith("<@"+self.data['self']['id']+">"):
 						slack.chat.post_message(message['channel'],"<@"+message['user']+">: Okej",as_user=True)
+					# Private message from someone?
 					elif message['channel'].startswith("D") and message['user']!=self.data['self']['id']:
 						slack.chat.post_message(message['channel'],"Okej",as_user=True)
-				print(message)
 		except KeyboardInterrupt:
 			pass
 
+		# Finalize
 		self.websocket.close()
 		self.connected = False
 
@@ -56,15 +69,10 @@ class SlackRtmConnectionError(Exception):
 	def __str__(self):
 		return repr(self.error)
 
-
-with open('SLACK_TOKEN','r') as slack_token_file:
-    slack_token = slack_token_file.read().replace('\n','')
-slack = Slacker(slack_token)
-server = Server(slack)
-server.rtm_connect()
-server.handle_loop()
-#ws.send("Hello, World")
-
-# Send messages
-#for line in sys.stdin:
-#	slack.chat.post_message('#bot',line,as_user=True)
+if __name__ == '__main__':
+	with open('SLACK_TOKEN','r') as slack_token_file:
+		slack_token = slack_token_file.read().replace('\n','')
+	slack = Slacker(slack_token)
+	server = Server(slack)
+	server.rtm_connect()
+	server.handle_loop()
